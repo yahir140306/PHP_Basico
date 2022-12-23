cdc536464192e5a2330b4186a798dfa8bf2f2da0
yahir140306 <95980218+yahir140306@users.noreply.github.com>
2022-12-22 19:27:25 -0600

    Codigo Sencillo - basico de php

---
+++ sintaxis-basica.php
@@ -0,0 +1,33 @@
+<?php
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+// echo "No, no. Yo soy " . $nombre . " " . $apellido . "\n";
+
+// echo "No, no. Yo soy $nombre $apellido \n";
+// echo 'No, no. Yo soy $nombre $apellido \n';
+
+// echo "El resultado de 4 * 5 es " . (4 * 5);
+
+$edades = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+
+// var_dump( $edades );
+// print_r( $edades );
+
+// Esto ya es un comentario
+# Esto ya es otro comentario
+
+/* 
+
+Cualquieeeer cosa
+En multiples
+Líneas
+
+*/
+
+echo "\n";
